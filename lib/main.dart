470a0394ab1d890642547f092c60f3b7fdccbe83
Nasirul Haque Ridoy <dpridoy@gmail.com>
2019-11-13 12:03:26 +0600

    second commit adding click event

--- lib/main.dart
+++ lib/main.dart
@@ -8,16 +8,47 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Hello World App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello World App'),
-        ),
-        body: Center(child: Text('Hello World', style: new TextStyle(
-          fontWeight: FontWeight.bold,
-          color: Colors.blue
-        ),),),
-      ),
+      home: ClickMeApp()
     );
   }
 }
 
+class ClickMeApp extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() =>ChangeText();
+
+}
+
+class ChangeText extends State<ClickMeApp>{
+
+  String defaultText= "Click me!!";
+  int i=0;
+
+  void changeText(){
+
+    setState(() {
+      if(i%2==0)
+        defaultText="You Clicked!!";
+      else
+        defaultText="Click me!!";
+      i++;
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:new Text('Hello World App'),
+      ),
+      body: Center(child:new Text(defaultText, style: new TextStyle(
+          fontWeight: FontWeight.bold,
+          color: Colors.blue),
+      ),),
+    floatingActionButton: FloatingActionButton(onPressed: changeText,
+      tooltip: 'Click Me',
+      child: Icon(Icons.mouse),),);
+  }
+
+}
